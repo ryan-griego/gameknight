4ae19318ac21b1c79bf1e22973904b959d12600c
Ryan Griego <ryangriego@gmail.com>
2020-09-10 16:27:01 +0000

    restarting feature 2

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from './header';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -19,11 +18,8 @@ export default class App extends React.Component {
   }
 
   render() {
-
-    return;
-    this.state.isLoading
+    return this.state.isLoading
       ? <h1>Testing connections...</h1>
       : <h1>{this.state.message}</h1>;
-
   }
 }
--- client/components/header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Header extends React.Component {
-  render() {
-    return <h1>THis is a header component</h1>;
-  }
-}
-
-export default Header;
