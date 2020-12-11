53053cf0e365d8637fed6343eb6bf185ce25b499
Ryan Griego <ryangriego@gmail.com>
2020-12-11 18:20:16 +0000

    fixed issue where video was autoplaying in full screen mode

--- client/components/header-video.jsx
+++ client/components/header-video.jsx
@@ -1,25 +1,45 @@
 import React from 'react';
 
 export default function HeaderVideo() {
+  if (screen.width > 1024) {
+    return (
+      <div>
+        <div className="videoContainer">
+          <div className="hero d-flex align-items-center justify-content-center">
+            <div className="overlay"></div>
 
-  return (
-    <div>
-      <div className="videoContainer">
-        <div className="hero d-flex align-items-center justify-content-center">
-          <div className="overlay"></div>
-
-          <div className="hero-text container d-flex flex-wrap justify-content-center">
-            <h1 className="col-12 text-uppercase p-0 text-center text-size-4 hero-text-header">Game On</h1>
-            <h3 className="col-12 text-center hero-text-sub-header">Our favorite games on sale</h3>
-          </div>
-          <div className="header-video">
-            <video autoPlay loop muted>
-              <source src="videos/board-zoom.mp4" type="video/mp4" />
-            </video>
+            <div className="hero-text container d-flex flex-wrap justify-content-center">
+              <h1 className="col-12 text-uppercase p-0 text-center text-size-4 hero-text-header">Game On</h1>
+              <h3 className="col-12 text-center hero-text-sub-header">Our favorite games on sale</h3>
+            </div>
+            <div className="header-video">
+              <video autoPlay loop muted>
+                <source src="videos/board-zoom.mp4" type="video/mp4" />
+              </video>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <div className="videoContainer">
+          <div className="hero d-flex align-items-center justify-content-center">
+            <div className="overlay"></div>
+
+            <div className="hero-text container d-flex flex-wrap justify-content-center">
+              <h1 className="col-12 text-uppercase p-0 text-center text-size-4 hero-text-header">Game On</h1>
+              <h3 className="col-12 text-center hero-text-sub-header">Our favorite games on sale</h3>
+            </div>
+            <div className="header-gif">
+              <img className="header-gif" src="images/mobile-board-zoom-sm.gif" />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+
+  }
 
 }
